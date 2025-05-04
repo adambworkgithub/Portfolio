9f4e20aebaa4f8462dcf4c5e2ff8e72d6e805966
Adam Bagnall <adambagnall0@gmail.com>
2025-05-04 12:28:12 -0600

    Final touches.

---
+++ init_skills.py
@@ -0,0 +1,41 @@
+from app import get_db_connection
+
+def initialize_skills():
+    skills = [
+        ('Python', 3),
+        ('Flask', 3),
+        ('PostgreSQL', 3),
+        ('JavaScript', 2),
+        ('CSS', 4),
+        ('Git', 4),
+        ('HTML', 4),
+        ('SQL', 4),
+        ('C', 3),
+        ('Figma', 3),
+        ('Canva', 3),
+        ('AutoDesk Fusion 360', 2),
+        ('Adobe Illustrator', 2),
+        ('Adobe Photoshop', 3),
+        ('Adobe XD', 3),
+        ('Arduino', 2),
+        ('Access', 3),
+        ('Excel', 4)
+    ]
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    # Insert initial skill ratings
+    for tech, proficiency in skills:
+        cur.execute("""
+            INSERT INTO tech_stack (technology, proficiency)
+            VALUES (%s, %s)
+            ON CONFLICT(technology) DO UPDATE SET proficiency=%s
+            """, (tech.lower(), proficiency, proficiency))
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+
+if __name__ == '__main__':
+    initialize_skills()
